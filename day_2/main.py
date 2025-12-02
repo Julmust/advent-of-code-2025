7eb660fc193e35c2a082fa62fcfe92aeb93f2488
Jesper Bergeskans <jesper.bergeskans@eye.security>
2025-12-02 09:11:27 +0100

    part 2, very inefficient

--- day_2/main.py
+++ day_2/main.py
@@ -24,6 +24,36 @@ def part_1(inp) -> int:
     return total
 
 
+def check_substr(stringified, num_len, i, stop) -> int:
+    for j in range(1, stop + 1):
+        substr = stringified[:j]
+
+        for x in range(j, len(stringified) + 1, j):
+            if substr != stringified[x : x + len(substr)]:
+                break
+            if x + len(substr) == num_len:
+                return i
+
+    return 0
+
+
+def part_2(inp) -> int:
+    total = 0
+    loop = 0
+
+    for id_range in inp:
+        loop += 1
+        for i in range(id_range[0], id_range[1] + 1):
+            stringified = str(i)
+            num_len = len(stringified)
+            stop = num_len // 2
+
+            # look for substrings
+            total += check_substr(stringified, num_len, i, stop)
+
+    return total
+
+
 def main() -> None:
     """Main entry point for day X solution."""
     with open("full.txt", "r") as file:
@@ -33,6 +63,7 @@ def main() -> None:
     data = [list(map(int, i)) for i in data]
 
     print(f"Part 1: {part_1(data)}")
+    print(f"Part 2: {part_2(data)}")
 
 
 if __name__ == "__main__":
