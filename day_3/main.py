f68819421ed44af3dcb44ecb820da015ce204bbd
Jesper Bergeskans <jesper.bergeskans@eye.security>
2025-12-03 10:39:33 +0100

    part 2

--- day_3/main.py
+++ day_3/main.py
@@ -27,8 +27,43 @@ def part_1(banks) -> int:
                 pos2 = joltage
                 curr_max = add_ints(pos1, pos2)
 
-        print(curr_max)
         total += curr_max
+    return total
+
+
+def find_highest(bank, to_find):
+    split_pos = to_find - 1
+
+    # Is this how I would want to do it? No. Does it work? Yes
+    if not split_pos:
+        find_high_in_bank = bank
+        high, high_idx = 0, 0
+        for idx, battery in enumerate(find_high_in_bank):
+            if int(battery) > high:
+                high, high_idx = int(battery), idx
+        return str(high)
+
+    # figure out the highest number, excluding the last 11
+    # this will be our starting point
+    find_high_in_bank = bank[:-split_pos]
+    high, high_idx = 0, 0
+    for idx, battery in enumerate(find_high_in_bank):
+        if int(battery) > high:
+            high, high_idx = int(battery), idx
+
+    # remove everything before and including the highest number
+    bank = bank[high_idx + 1 :]
+
+    return str(high) + find_highest(bank, to_find - 1)
+
+
+def part_2(banks):
+    total = 0
+
+    for bank in banks:
+        res = find_highest(bank, 12)
+        total += int(res)
+
     return total
 
 
@@ -39,6 +74,7 @@ def main() -> None:
         data = data.split("\n")
 
     print(f"Part 1: {part_1(data)}")
+    print(f"Part 2: {part_2(data)}")
 
 
 if __name__ == "__main__":
